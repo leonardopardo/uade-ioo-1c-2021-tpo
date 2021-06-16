c374a88e4438005f266396a2af49fb9922c07404
Franco Levigna <Franco.Levigna@endava.com>
2021-06-16 18:50:06 -0300

    added getinstance method

--- src/controllers/ProveedorController.java
+++ src/controllers/ProveedorController.java
@@ -1,11 +1,37 @@
 package controllers;
 
-import dto.FacturaDTO;
-import dto.ProveedorDTO;
+import dto.ProveedorCompulsaDTO;
 import modelos.Proveedor;
+
 
 import java.util.List;
 
 public class ProveedorController {
     private List<Proveedor> proveedores;
+
+    private static ProveedorController instance;
+
+
+    public ProveedorCompulsaDTO obtener(String cuit) {
+
+        ProveedorCompulsaDTO dto = new ProveedorCompulsaDTO();
+
+        for (Proveedor proveedor : this.proveedores) {
+            if (proveedor.getCuit().equals(cuit)) {
+                dto.razonSocial = proveedor.getRazonSocial();
+                dto.cuit = proveedor.getCuit();
+                return dto;
+            }
+        }
+
+        return null;
+    }
+
+    public static ProveedorController getInstance() throws Exception {
+        if (instance == null) {
+            instance = new ProveedorController();
+        }
+        return instance;
+    }
+
 }
