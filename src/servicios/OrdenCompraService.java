a4a20589ddd186278e8e5679ad59cf48fab3090e
Franco Levigna <Franco.Levigna@endava.com>
2021-06-17 20:34:58 -0300

    OrdenCompraService fixed

--- src/servicios/OrdenCompraService.java
+++ src/servicios/OrdenCompraService.java
@@ -1,13 +1,13 @@
 package servicios;
 
-import modelos.Usuario;
+import modelos.OrdenCompra;
 import vendor.GenericDAO;
 
 public class OrdenCompraService extends GenericDAO {
 
 
     public OrdenCompraService() throws Exception {
-        super(Usuario.class, "./src/json/ordenesCompra.json");
+        super(OrdenCompra.class, "./src/json/ordenesCompra.json");
     }
 
     public int getProximoNumero() throws Exception {
