733ac7a764a4557976ad412a6550d1da8aada6a9
Franco Levigna <Franco.Levigna@endava.com>
2021-06-17 14:59:32 -0300

    DocumentosController first version

---
+++ src/controllers/DocumentosController.java
@@ -0,0 +1,60 @@
+package controllers;
+
+import dto.DetalleDTO;
+import dto.OrdenCompraDTO;
+import modelos.Detalle;
+import modelos.OrdenCompra;
+import modelos.Factura;
+import servicios.FacturaService;
+import servicios.OrdenCompraService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DocumentosController {
+
+    public static DocumentosController instance;
+    private ProveedorController pController;
+    private PrecioController precController;
+    private List<OrdenCompra> ordenesCompra;
+    private List<Factura> facturas;
+    private OrdenCompraService ordenCompraService;
+    private FacturaService facturaService;
+
+
+
+    protected DocumentosController() throws Exception {
+        this.ordenCompraService = new OrdenCompraService();
+        this.facturaService = new FacturaService();
+        this.ordenesCompra = new ArrayList<OrdenCompra>();
+        this.facturas = new ArrayList<Factura>();
+        this.pController = ProveedorController.getInstance();
+        this.precController = PrecioController.getInstance();
+    }
+
+
+    public static DocumentosController getInstance() throws Exception {
+        if (instance == null) {
+            instance = new DocumentosController();
+        }
+        return instance;
+    }
+
+
+    public void agregarOrdenCompra(OrdenCompraDTO dto) throws Exception {
+        OrdenCompra oc = new OrdenCompra(dto);
+        this.ordenesCompra.add(oc);
+        oc.setProveedor(this.pController.obtener(dto.cuitProveedor));
+        oc.setNumero(ordenCompraService.getProximoNumero());
+
+        for (DetalleDTO d : dto.detalles
+        ) {
+            Detalle nuevoDetalle = new Detalle(d);
+        }
+
+    }
+
+}
+
+
+
