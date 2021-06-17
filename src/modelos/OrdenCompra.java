37f40939108226e1cf9cd5832291ced95281b1ea
Franco Levigna <Franco.Levigna@endava.com>
2021-06-17 15:06:28 -0300

    added properties & setNumero, setDetalle, setProveedor, OrdenCompra, Constructor methods

--- src/modelos/OrdenCompra.java
+++ src/modelos/OrdenCompra.java
@@ -1,10 +1,41 @@
 package modelos;
 
+import dto.OrdenCompraDTO;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 public class OrdenCompra extends Documento {
 
-    public OrdenCompra(){
-        this.detalles = new ArrayList<>();
+    private int numero;
+    private LocalDateTime fecha;
+    private String descripcion;
+    private List<Detalle> detalles;
+    private Double monto;
+    private Proveedor proveedor;
+
+    public OrdenCompra(OrdenCompraDTO dto) {
+        this.numero = dto.numero;
+        this.fecha = dto.fecha;
+        this.descripcion = dto.descripcion;
+        this.monto = dto.monto;
+        this.detalles = new ArrayList<Detalle>();
     }
+
+    public void setProveedor(Proveedor p) {
+        this.proveedor = p;
+    }
+
+
+    public void setDetalle(Detalle detalle) {
+        this.detalles.add(detalle);
+    }
+
+
+    public void setNumero(int num) {
+        this.numero = num;
+    }
+
+
 }
