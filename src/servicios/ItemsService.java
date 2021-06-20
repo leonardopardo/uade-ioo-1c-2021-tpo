6c2d8c0b96b4723645de018fdf64637c9fd3468b
Franco Levigna <Franco.Levigna@endava.com>
2021-06-20 01:36:42 -0300

    Formatted code in ItemsService

--- src/servicios/ItemsService.java
+++ src/servicios/ItemsService.java
@@ -4,7 +4,9 @@ import modelos.OrdenCompra;
 import vendor.GenericDAO;
 
 public class ItemsService extends GenericDAO {
+
     public ItemsService() throws Exception {
         super(OrdenCompra.class, "./src/json/items.json");
     }
+
 }
