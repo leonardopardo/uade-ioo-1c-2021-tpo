1604ceddff4d5081e350a2418446a4e6ae9e45f2
Franco Levigna <Franco.Levigna@endava.com>
2021-06-20 01:29:11 -0300

    Code formatted

--- src/dto/ItemDTO.java
+++ src/dto/ItemDTO.java
@@ -5,9 +5,11 @@ import modelos.enums.TipoItem;
 import modelos.enums.Unidad;
 
 public class ItemDTO {
+
     public String codigo;
     public String titulo;
     public Rubro rubro;
     public Unidad unidad;
     public TipoItem tipo;
+
 }
