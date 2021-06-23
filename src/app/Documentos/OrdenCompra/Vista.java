f57bb421fee576b11cd2245344ac666dfea23174
Leonardo <leonardo_pardo@hotmail.com>
2021-06-23 17:31:41 -0300

    documentos y ordenes de compra

--- src/app/Documentos/OrdenCompra/Vista.java
+++ src/app/Documentos/OrdenCompra/Vista.java
@@ -1,4 +1,16 @@
 package app.Documentos.OrdenCompra;
 
-public class Vista {
+import javax.swing.*;
+
+public class Vista extends JPanel {
+    private JPanel pnlMain;
+    private JTable tableOrdenesCompra;
+    private JComboBox comboBoxProveedores;
+    private JButton buscarButton;
+    private JButton nuevaOrdenButton;
+    private JButton cancelarButton;
+
+    public Vista(){
+        this.add(this.pnlMain);
+    }
 }
