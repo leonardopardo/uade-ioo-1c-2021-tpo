aff5e78c1b7846d7cf3d64283d55d00446c47486
ENDAVA\flevigna <levignafranco@gmail.com>
2021-06-28 02:23:52 -0300

    returns modified & added constructor

--- src/modelos/CertificadoExcencion.java
+++ src/modelos/CertificadoExcencion.java
@@ -1,5 +1,6 @@
 package modelos;
 
+import dto.CertificadoDTO;
 import modelos.enums.TipoRetencion;
 
 import java.time.LocalDate;
@@ -12,19 +13,26 @@ public class CertificadoExcencion {
     private TipoRetencion tipo;
     private Float alicuota;
 
+    public CertificadoExcencion(CertificadoDTO certifDTO) {
+        this.fechaInicio = certifDTO.fechaInicio;
+        this.fechaFin = certifDTO.fechaFin;
+        this.tipo = certifDTO.tipo;
+        this.alicuota = certifDTO.alicuota;
+    }
+
     public LocalDate getFechaInicio() {
-        return fechaInicio;
+        return this.fechaInicio;
     }
 
     public LocalDate getFechaFin() {
-        return fechaFin;
+        return this.fechaFin;
     }
 
     public TipoRetencion getTipo() {
-        return tipo;
+        return this.tipo;
     }
 
     public Float getAlicuota() {
-        return alicuota;
+        return this.alicuota;
     }
 }
